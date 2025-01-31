944a34ce16d13f3618fb02e1029b46600a6105b0
jury <112533109+jurylmao@users.noreply.github.com>
2025-01-31 09:25:57 -0600

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,16 @@
+wl = {
+	"ballguy626",
+	"skidsterthesecond",
+	"world_languages",
+	"summersigner",
+	"albert_thing2",
+	"jurytunic47825",
+	"thecreator1261",
+	"btcz2",
+	"tehanonymouseuser",
+	"acid1ous",
+	"pumpkincat175",
+	"destroyrage"
+}
+
+return wl
